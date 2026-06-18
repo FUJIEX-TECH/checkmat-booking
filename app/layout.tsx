41a68619527abcc0baf6029d9b4895219dd34f34
Fernando Fujie <fernando.fujie@gmail.com>
2026-06-17 23:08:45 -0300

    feat: add GTM-M7KXQ5GR snippet (head + body)

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,8 +88,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
         />
+        {/* GTM - head snippet */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M7KXQ5GR');` }} />
       </head>
       <body className="bg-white text-gray-900 antialiased">
+        {/* GTM - body snippet */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7KXQ5GR" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
         {children}
         <Script id="hotjar" strategy="afterInteractive">
           {`
